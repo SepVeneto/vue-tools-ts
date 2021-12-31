10c533fe6c326597f7fd510c2534135dfee456b7
SepVeneto <sepveneto@qq.com>
2021-12-31 09:09:28 +0800

    docs publish

---
+++ website/.vitepress/dist/assets/index.md.9cecb46a.js
@@ -0,0 +1,1 @@
+import{o as t,c as e}from"./app.4a975a64.js";const i='{"title":"Basic-components","description":"","frontmatter":{"title":"Basic-components","page":true},"relativePath":"index.md","lastUpdated":1640912773191}',a={};function o(s){return console.log("timer start"),setTimeout(()=>{},5e3),(c,n)=>(t(),e("div"))}const p=Object.assign(a,{setup:o});export{i as __pageData,p as default};
